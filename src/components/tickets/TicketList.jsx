7685bd6230a5aa581fbd310e91f09816e4c802cd
Jason LeGlue <jasonleglue@gmail.com>
2026-06-02 21:19:47 -0500

    Created search bar with search functionality. Moved all of the ticket filter bar elements into TicketFilterBar.jsx

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { getAllTickets } from "../../services/ticketService.js";
 import { Ticket } from "./Ticket.jsx";
 import "./Tickets.css";
+import { TicketFilterBar } from "./TicketFilterBar.jsx";
 
 export const TicketList = () => {
   const [allTickets, setAllTickets] = useState([]);
   const [showEmergencyOnly, setShowEmergencyOnly] = useState(false);
   const [filteredTickets, setFilteredTickets] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     getAllTickets().then((ticketsArray) => {
@@ -27,30 +29,23 @@ export const TicketList = () => {
     console.log("show emergency changes");
   }, [showEmergencyOnly, allTickets]);
 
+  useEffect(() => {
+    const foundTickets = allTickets.filter((ticket) =>
+      ticket.description.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredTickets(foundTickets);
+  }, [searchTerm, allTickets]);
+
   return (
     <div className="tickets-container">
       <h2>Tickets</h2>
-      <div>
-        <button
-          className="filter-btn btn-primary"
-          onClick={() => {
-            setShowEmergencyOnly(true);
-          }}
-        >
-          Emergency
-        </button>
-        <button
-          className="filter-btn btn-info"
-          onClick={() => {
-            setShowEmergencyOnly(false);
-          }}
-        >
-          Show All
-        </button>
-      </div>
+      <TicketFilterBar
+        setShowEmergencyOnly={setShowEmergencyOnly}
+        setSearchTerm={setSearchTerm}
+      />
       <article className="tickets">
         {filteredTickets.map((ticketObj) => {
-          return <Ticket ticket={ticketObj} key={ticketObj.id} name="Joe" />;
+          return <Ticket ticket={ticketObj} key={ticketObj.id} />;
         })}
       </article>
     </div>
