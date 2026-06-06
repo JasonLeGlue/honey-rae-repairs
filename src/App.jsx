82fa31f1df3a472390b447fdd817b3c63cc81319
Jason LeGlue <jasonleglue@gmail.com>
2026-06-06 09:43:48 -0500

    Added EmployeeList.jsx and Employees.css to display and style page of employees

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { TicketList } from "./components/tickets/TicketList.jsx";
 import { CustomerList } from "./components/customers/CustomersList.jsx";
+import { EmployeeList } from "./components/employees/EmployeeList.jsx";
 
 export const App = () => {
   return (
     <>
       {/*<TicketList />*/}
-      <CustomerList />
+      {/*<CustomerList />*/}
+      <EmployeeList />
     </>
   );
 };
---
+++ src/components/employees/EmployeeList.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { User } from "../../users/User.jsx";
+import { getStaffUsers } from "../../services/userService.jsx";
+import "./Employees.css";
+export const EmployeeList = () => {
+  //make list of employees an empty useState
+  //use Effect to populate employees state from getStaffUsers
+  //use .map() on state?, pass that as user prop for User function
+
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    getStaffUsers().then((employeesArray) => {
+      setEmployees(employeesArray);
+    });
+  }, []);
+
+  return (
+    <div className="employees">
+      {employees.map((employeeObj) => {
+        return <User user={employeeObj} />;
+      })}
+    </div>
+  );
+};
