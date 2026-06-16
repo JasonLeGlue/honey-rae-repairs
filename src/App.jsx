eda5a058686f73c7c82598ed2480e8d3712d4f6b
Jason LeGlue <jasonleglue@gmail.com>
2026-06-16 18:53:13 -0500

    Added NavBar.jsx and NavBar.css, and some routing to main

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import "./App.css";
 import { TicketList } from "./components/tickets/TicketList.jsx";
 import { CustomerList } from "./components/customers/CustomersList.jsx";
 import { EmployeeList } from "./components/employees/EmployeeList.jsx";
+import { Route, Routes, Outlet } from "react-router-dom";
+import { NavBar } from "./components/nav/NavBar.jsx";
 
 export const App = () => {
   return (
-    <>
-      {/*<TicketList />*/}
-      {/*<CustomerList />*/}
-      <EmployeeList />
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="customers" element={<CustomerList />} />
+      </Route>
+    </Routes>
   );
 };
