7ff5da8daa0e893f04efe3e6b851fbf3367e0aa1
Jason LeGlue <jasonleglue@gmail.com>
2026-06-20 09:54:15 -0500

    Added customerService.js and CustomerDetails.jsx. Customer's information now renders when you click on them in /customers

--- src/components/customers/CustomerDetails.jsx
+++ src/components/customers/CustomerDetails.jsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { getCustomerByUserId } from "../../services/customerService.js";
+import "./Customers.css";
 
 export const CustomerDetails = () => {
   // /customer/3
   // path="/customers/:customerId"
 
+  const [customer, setCustomer] = useState({});
   const { customerId } = useParams();
 
-  return <div>Customer #{customerId}</div>;
+  useEffect(() => {
+    getCustomerByUserId(customerId).then((data) => {
+      const customerObj = data[0];
+      setCustomer(customerObj);
+    });
+  }, [customerId]);
+
+  return (
+    <section className="customer">
+      <header className="customer-header">{customer.user?.fullname}</header>
+      <div>
+        <span className="customer-info">Email: </span>
+        {customer.user?.email}
+      </div>
+      <div>
+        <span className="customer-info">Address: </span>
+        {customer.address}
+      </div>
+      <div>
+        <span className="customer-info">Phone Number: </span>
+        {customer.phoneNumber}
+      </div>
+    </section>
+  );
 };
